494b878382edf26d391e917c1bbf9f7d274e8aeb
Asim <asim005011997@gmail.com>
2021-05-31 17:07:22 +0530

    added header, react-typed, react-particles-js and media queries for different screens of the device(responsive)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import './App.css';
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import Particles from 'react-particles-js'
 
 function App() {
   return (
       <>
+        <Particles 
+        className="particle-canvas"
+        params={{
+          particles:{
+            number:{
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape:{
+              type: 'star',
+              stroke: {
+                width: 7,
+                color:'#f9ab00'
+              }
+            }
+          }
+        }}
+        />
         <Navbar />
+        <Header />
       </>
   );
 }
